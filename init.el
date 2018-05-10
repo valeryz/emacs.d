ec87883e73f798d5a556d4562ffd211a9502e272
Valeriy Zamarayev <valeriy.zamarayev@gmail.com>
2018-05-10 20:06:46 +0200

    multiple cursors settings

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 ;;; (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
 (setenv "GOROOT" "/home/vz/go1.8")
-(setenv "GOHOME" "/home/vz")
+(setenv "GOPATH" "/home/vz")
 (setenv "GOBIN" "/home/vz/gobin")
 (setenv "PATH" (concat (getenv "PATH")
                        ":" (getenv "GOROOT") "/bin"
@@ -192,3 +192,10 @@
 (put 'erase-buffer 'disabled nil)
 
 (global-subword-mode)
+
+;; Multiple cursors
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
